fceff9d7177d1f1222b703be78fa3c6d08b43ae9
mehadi <mehadishisir@gmail.com>
2026-02-24 22:03:33 +0600

    add all setup of express and node js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 const port = 3000;
+
+require("dotenv").config();
+
+// middleware
+
+app.use(express.json());
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Cine Nest server is ready");
 });
